0e379bae256bfecf30c0e8185697aab4edca7a0e
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2023-03-06 23:31:08 -0600

    Fix de script de publicación

--- publish.py
+++ publish.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 import begin
 import re
+import os
 
 @begin.start(auto_convert=True)
 def main(file_name):
@@ -16,9 +17,9 @@ def main(file_name):
     with open(file_name, "r+") as f:
         content = f.read()
         content =  re.sub("date: \d{4}-\d{2}-\d{2}", f"date: {date}", content)
-        print(content)
-        # f.seek(0)
-        # f.write(content)
+        # print(content)
+        f.seek(0)
+        f.write(content)
 
-    # new_name = file_name.replace("_drafts/", f"_posts/{date}-")
-    # os.rename(f"_drafts/{file_name}", new_name)
+    new_name = file_name.replace("_drafts/", f"_posts/{date}-")
+    os.rename(file_name, new_name)
