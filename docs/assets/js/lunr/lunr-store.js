0fe6d73b49cb9926628f3125d953905ca2216d45
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2023-04-07 19:17:36 -0600

    Nuevo post publicado: separación de código

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -652,4 +652,10 @@ var store = [{
         "tags": ["solid","principios-solid","isp"],
         "url": "/2023/04/01/el-principio-de-segregacion-de-interfaces.html",
         "teaser": "https://res.cloudinary.com/hectorip/image/upload/c_scale,w_400/v1669958588/3018709125_star_explosion_Highly_detailed__surrealism__trending_on_art_station__triadic_color_scheme__smooth__sharp_focus__matte__elegant__the_most_beautiful_image_ever_seen__illustration__digital_paint__dark__gloomy__octane_render__8k__4k__aexjen.png"
+      },{
+        "title": "Cuando separar el código",
+        "excerpt":"Hay quienes afirman que mientras más pequeñas sean tus clases o funciones, mejor. Hasta llegan a poner un límite de la líneas que pueden ir en un método o función. Esto crea código súper fragmentado que llega a ser complejo por el simple hecho de tener muchos elementos individuales que...","categories": [],
+        "tags": ["aposd","ousterhout","diseño-de-software"],
+        "url": "/2023/04/07/cuando-separar-el-codigo.html",
+        "teaser": "https://res.cloudinary.com/hectorip/image/upload/c_scale,w_400/v1680411928/risto-kokkonen-HAIDBanzi8o-unsplash_okktgd.jpg"
       }]
