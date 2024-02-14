d4cfa75c5fd0b0d8bcfefa2c141d0961712fe877
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2024-02-14 17:51:19 -0600

    blog adsesnse

---
+++ docs/publish_tools/target/debug/deps/publish_tools-15cb81a5a11bfe4f.d
@@ -0,0 +1,5 @@
+/Users/hectorip/Development/thedojomx/TheDojoMXBlog/publish_tools/target/debug/deps/publish_tools-15cb81a5a11bfe4f.rmeta: src/main.rs
+
+/Users/hectorip/Development/thedojomx/TheDojoMXBlog/publish_tools/target/debug/deps/publish_tools-15cb81a5a11bfe4f.d: src/main.rs
+
+src/main.rs:
--- docs/publish_tools/target/debug/deps/terminal_size-0fd8dc5213799760.d
+++
@@ -1,6 +0,0 @@
-/Users/hectorip/Development/thedojo/TheDojoMXBlog/publish_tools/target/debug/deps/terminal_size-0fd8dc5213799760.rmeta: /Users/hectorip/.cargo/registry/src/github.com-1ecc6299db9ec823/terminal_size-0.1.17/src/lib.rs /Users/hectorip/.cargo/registry/src/github.com-1ecc6299db9ec823/terminal_size-0.1.17/src/unix.rs
-
-/Users/hectorip/Development/thedojo/TheDojoMXBlog/publish_tools/target/debug/deps/terminal_size-0fd8dc5213799760.d: /Users/hectorip/.cargo/registry/src/github.com-1ecc6299db9ec823/terminal_size-0.1.17/src/lib.rs /Users/hectorip/.cargo/registry/src/github.com-1ecc6299db9ec823/terminal_size-0.1.17/src/unix.rs
-
-/Users/hectorip/.cargo/registry/src/github.com-1ecc6299db9ec823/terminal_size-0.1.17/src/lib.rs:
-/Users/hectorip/.cargo/registry/src/github.com-1ecc6299db9ec823/terminal_size-0.1.17/src/unix.rs:
